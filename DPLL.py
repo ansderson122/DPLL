3f8c5218a083b8dc158ad985810f657d67f5760b
Julyanderson Alves <ansderson84001@gmail.com>
2023-08-12 09:20:48 -0300

    simplifica

--- DPLL.py
+++ DPLL.py
@@ -7,9 +7,22 @@ dados = []
 with open(PATH, 'r') as entrada:
     for linhas in entrada:
         if linhas[0] == "p": continue
-        clausula = []
-        for literal in linhas.split(" "):
-            if int(literal) != 0: clausula.append(int(literal))
+        clausula = linhas.split(" ")
+        clausula = [int(literal) for literal in clausula if literal != "0\n"]
         dados.append(clausula)
 
+def simplifica(f :list)-> None:
+    #escolhe um literal 
+    a = 10 #aqui dever ser chamado uma função para escolhe um literal
+
+    for clausula in reversed(f):
+        for literal in clausula:
+            if a == literal:
+                f.remove(clausula)
+                break
+            elif -a == literal:
+                clausula.remove(literal)
+  
+
+simplifica(dados)
 print(dados)
