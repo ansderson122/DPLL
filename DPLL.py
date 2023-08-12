d72151d3f4e124427ff05c4d4548c24e365ea7c2
Julyanderson Alves <ansderson84001@gmail.com>
2023-08-12 11:04:05 -0300

    problema na recursão

--- DPLL.py
+++ DPLL.py
@@ -1,4 +1,4 @@
-
+import copy
 PATH = 'entrada.txt'
 
 
@@ -13,6 +13,7 @@ with open(PATH, 'r') as entrada:
         dados.append(clausula)
 
 def simplifica(f :list, a:int)-> None:
+    global dados
     for clausula in reversed(f):
         for literal in clausula:
             if a == literal:
@@ -20,18 +21,46 @@ def simplifica(f :list, a:int)-> None:
                 break
             elif -a == literal:
                 clausula.remove(literal)
-    
+                if len(clausula) == 0: # clausala vazia 
+                    f = copy.deepcopy(dados)
+                    res.remove(a)
+                    res.append(-a)
+                    simplifica(f,-a)
+                    return
+
+    dados = copy.deepcopy(f)
     print(f)
+
+    # testa se ha duas clausulas unitaria com sinais diferente 
+    for i in range(len(f)):
+        for j in range(i+1,len(f)):
+            if len(f[i]) > 1: # não uma clausula unitaria 
+                continue
+            if len(f[j]) > 1:
+                continue
+
+            if f[i][0] == -f[j][0]:
+                print("É insatisfativel")
+                return
+            
+        
+       
     for clausula in f:
         if len(clausula) == 1:
             res.append(clausula[0])
             simplifica(f, clausula[0])
+        
 
 
 
 def DPLL():
-    res.append(10) #aqui dever ser chamado uma função para escolhe um literal
-    simplifica(dados,res[0])
+    res.append(4) #aqui dever ser chamado uma função para escolhe um literal
+    f = copy.deepcopy(dados)
+    continua = True
+    
+    simplifica(f,res[0])
+    
+
     print("res : " + str(res))
 
 
